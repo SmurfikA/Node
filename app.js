c6344f10a3b5924ed0fa707eb7c5e72f88dec141
Sergey-T-ban <s.trunka@e-u.edu.ua>
2025-05-26 19:50:37 +0300

    first

--- app.js
+++ app.js
@@ -23,14 +23,6 @@ const limiter = RateLimit({
   max: 20,
 });
 
-const connectDB = async () => {
-  try {
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log('MongoDB connected successfully');
-  } catch (err) {
-      console.error('Database connection failed:', err);
-  }
-};
 
 
 
